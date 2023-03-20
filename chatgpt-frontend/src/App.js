f516789c716c45c625e28df08388462bbd65b36d
fatihbaltaci <baltacifatih14@gmail.com>
2023-03-21 02:40:27 +0300

    Update

--- chatgpt-frontend/src/App.js
+++ chatgpt-frontend/src/App.js
@@ -1,119 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+
+import axios from "axios";
+import "./App.css";
+
+const baseURL = "http://localhost:8090";
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
   const [chats, setChats] = useState([]);
-  const [selectedChat, setSelectedChat] = useState(null);
+  const [selectedChatId, setSelectedChatId] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     fetchChats();
   }, []);
 
+  useEffect(() => {
+    if (selectedChatId) {
+      fetchMessages(selectedChatId);
+    } else {
+      setMessages([]);
+    }
+  }, [selectedChatId]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "auto" });
+  };
+
   const fetchChats = async () => {
-    const response = await fetch('http://localhost:8090/api/chat_history/', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
+    try {
+      const response = await axios.get(`${baseURL}/api/chats/`);
+      setChats(response.data);
+    } catch (error) {
+      console.error("Error fetching chats:", error);
+    }
+  };
+
+  const fetchMessages = async (chatId) => {
+    try {
+      const response = await axios.get(`${baseURL}/api/chats/${chatId}/`);
+      setMessages(response.data);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  };
+
+  const sendMessage = async () => {
+    if (!selectedChatId) {
+      alert("Please select a chat before sending a message.");
+      return;
+    }
+
+    // Update the local state before sending the message to the backend
+    setMessages([
+      ...messages,
+      {
+        content: inputMessage,
+        role: "user",
       },
-    });
+    ]);
+    setInputMessage("");
 
-    const fetchedChats = await response.json();
-    setChats(fetchedChats);
-  };
-
-  const fetchChatMessages = async (chatId) => {
-    const response = await fetch(`http://localhost:8090/api/chat_messages/${chatId}/`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    const chatMessages = await response.json();
-    const formattedMessages = chatMessages.map((msg) => ({
-      type: msg.role,
-      content: msg.content,
-    }));
-    setMessages(formattedMessages);
-  };
-
-  const selectChat = (chatId) => {
-    setSelectedChat(chatId);
-    fetchChatMessages(chatId);
-  };
-
-  const clearChatDisplay = () => {
-    setMessages([]);
-    setSelectedChat(null);
-  };
-
-  const submitMessage = async (e) => {
-    e.preventDefault();
-
-    if (!message.trim()) return;
-
-    setMessages([...messages, { type: 'user', content: message }]);
-    setMessage('');
-
-    const response = await fetch('http://localhost:8090/api/chat/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message }),
-    });
-
-    const data = await response.json();
-    setMessages((prevMessages) => [...prevMessages, { type: 'ai', content: data.message }]);
-    fetchChats();
-  };
-
-  const handleKeyDown = (e) => {
-    if ((e.metaKey || e.ctrlKey) && e.key === 'Enter') {
-      submitMessage(e);
+    try {
+      await axios.post(`${baseURL}/api/chat/`, {
+        chat_id: selectedChatId,
+        message: inputMessage,
+      });
+      fetchMessages(selectedChatId);
+    } catch (error) {
+      console.error("Error sending message:", error);
     }
   };
 
   return (
     <div className="App">
-      <div className="sidebar">
-        <button onClick={clearChatDisplay}>New Chat</button>
+      <div className="chat-container">
         <div className="chat-history">
-          {chats.map((chat, idx) => (
-            <div key={idx} className="chat-item" onClick={() => selectChat(chat.id)}>
-              Chat {idx + 1}
+          {chats.map((chat) => (
+            <div
+              key={chat.id}
+              onClick={() => setSelectedChatId(chat.id)}
+              className={`chat ${selectedChatId === chat.id ? "selected" : ""}`}
+            >
+              Chat {chat.id}
             </div>
           ))}
         </div>
-      </div>
-      <div className="chat-container">
-        {selectedChat ? (
-          messages.map((msg, idx) => (
-            <div
-              key={idx}
-              className={`chat-message ${msg.type === 'user' ? 'user-message' : 'ai-message'}`}
-            >
-              <span className="message-content">{msg.content}</span>
-            </div>
-          ))
-        ) : (
-          <div className="no-chat-selected">No chat selected</div>
-        )}
-      </div>
-      <form onSubmit={submitMessage}>
-        <textarea
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyDown={handleKeyDown}
-          placeholder="Type your message here..."
-          rows={3}
+        <div className="chat-ui">
+          <div className="chat-messages">
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={`message ${message.role === "user" ? "user" : "assistant"}`}
+              >
+                {message.content}
+              </div>
+            ))}
+            <div ref={messagesEndRef}></div>
+          </div>
+          <div className="chat-input">
+            <input
+              type="text"
+              placeholder="Type a message"
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.target.value)}
             />
-            <button type="submit">Send</button>
-          </form>
+            <button onClick={sendMessage} disabled={!inputMessage}>
+              Send
+            </button>
+          </div>
         </div>
-        );
-      }
-      
-      export default App;
+      </div>
+    </div>
+  );
+}
+
+export default App;
