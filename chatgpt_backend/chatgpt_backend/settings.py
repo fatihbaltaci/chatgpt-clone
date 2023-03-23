59713f53b00edaf1f08ca5eed870610fd55117b0
fatihbaltaci <baltacifatih14@gmail.com>
2023-03-24 01:59:49 +0300

    Update static

--- chatgpt_backend/chatgpt_backend/settings.py
+++ chatgpt_backend/chatgpt_backend/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'api/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'api/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
